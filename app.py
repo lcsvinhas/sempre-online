54a890c1aaa324a01fe889de6befbe00e9a755f6
lcsvinhas <l.vinhas.lv@gmail.com>
2025-03-20 15:07:26 -0300

    adicionei temporizador

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ import ctypes
 import time
 
 mouse_event = ctypes.windll.user32.mouse_event
+contador = 0
 
 MOUSE_LEFTDOWN = 0x02
 MOUSE_LEFTUP = 0x04
 
-while True:
+temporizador = int(input("Digite quantos minutos deseja rodar o programa: "))
+
+while temporizador > 0:
+    print("\033[H\033[J", end="")
+    print("-=-=-=-=- \033[92mSempre Online\033[0m -=-=-=-=-")
+    print(f"\033[92m{temporizador}\033[0m minuto(s) restante(s)")
+    print(f"\033[92m{contador}\033[0m clique(s) efetuado(s)")
+    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
     time.sleep(60)
     mouse_event(MOUSE_LEFTDOWN, 0, 0, 0, 0)
     mouse_event(MOUSE_LEFTUP, 0, 0, 0, 0)
-    print("Clique realizado!")
+    contador += 1
+    temporizador -= 1
+
+print("Programa encerrado, obrigado pela preferência!")
